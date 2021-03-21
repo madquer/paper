0369d1c3821a2c98d416afe851658cfd07f86efe
SEOJUNGMIN <62593594+madquer@users.noreply.github.com>
2021-03-21 14:31:38 +0900

    Update vgg.py

--- vision/vgg.py
+++ vision/vgg.py
@@ -233,4 +233,11 @@ time : 8.51s
 
 """
 
+"""comment
+parameters : 143,667,240
+nn.Linear(Fully connected layer) 층에서 약 120,000,000개의 파라미터가 생성됨.
+이 파라미터의 개수를 추후에 다른 모델에서 어떤 방식으로 감소시킬까? 
+추후 적용하는 'Global Average Pooling' 은 단순 Flatten 하는 것에 비해서 파라미터와 성능 관점에서 어떤, 어느정도의 영향을 미칠까?
+
+"""
 
